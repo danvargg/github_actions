e7907eda4f1d32fb91fe934221fecfb6ee6af6f7
Daniel Vargas <devg28@gmail.com>
2023-04-12 18:28:20 -0400

    code clean-up.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,7 +4,6 @@ chmod +x ./linux/hello-server
 
 sleep 3
 
-for i in Anakin Luke Leia Kylo Rey;
-do
-    echo "$(date): $(curl -s http://localhost:11000/${i})"
+for i in Anakin Luke Leia Kylo Rey; do
+  echo "$(date): $(curl -s http://localhost:11000/${i})"
 done
